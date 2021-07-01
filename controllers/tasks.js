65d59bdd0e146c8690afb42ddd36d995b8599a34
adw-ait <adwaityugu@gmail.com>
2021-07-01 09:19:06 +0530

    fixes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -38,11 +38,10 @@ const updateTask = async (req, res) => {
       new: true,
       runValidators: true,
     });
-    // if (!task) {
-    //   return res.status(404).json({ msg: `No Task with id ${taskID}` });
-    // }
+    if (!task) {
+      return res.status(404).json({ msg: `No Task with id ${taskID}` });
+    }
     res.status(200).json({ task });
-    // res.status(200).json({ id: taskID, data });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
